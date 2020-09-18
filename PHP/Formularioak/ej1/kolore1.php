23632f52a8a89c44f8946cde0d56018c3cacc755
AitorBasarrate <46562527+AitorBasarrate@users.noreply.github.com>
2020-09-18 14:06:44 +0200

    Add files via upload

---
+++ PHP/Formularioak/ej1/kolore1.php
@@ -0,0 +1,11 @@
+
+<form action="kolore2.php" method="POST">
+    <select name="koloreak">
+        <option value="#FF0000">Gorria</option>
+        <option value="#001AFF">Urdina</option>
+        <option value="#2CFF00">Berdea</option>
+        <option value="#000000">Beltza</option>
+        <option value="#FFFFFF">Zuria</option>
+    </select>
+    <input type="submit" name="bidali" value="Bidali"></input>
+</form>
---
+++ PHP/Formularioak/ej1/kolore2.php
@@ -0,0 +1,6 @@
+<?php
+    if(!isset($_GET["bidali"])){
+        $hautatutakoKolorea = $_POST["koloreak"];
+        echo "<body style=\"background-color:$hautatutakoKolorea\">";  
+    }
+?>
